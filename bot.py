0eba1aa2cd7f4d33c6310b07f20fb3e5af4eae87
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-07-03 13:16:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6202509183:AAFWv7jgUeY96gzhdqthVG1vOIxxAJq5jKc")
 API_ID = int(os.environ.get("API_ID", "21203142"))
-API_HASH = os.environ.get("API_HASH", "64d92910619503d54fdb9d89ccb805a6)
+API_HASH = os.environ.get("API_HASH", "64d92910619503d54fdb9d89ccb805a6")
 
 if __name__ == "__main__" :
     plugins = dict(
